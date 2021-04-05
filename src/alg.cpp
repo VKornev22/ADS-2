b15fc86437fda0a5bc6e7113e20885ff3ccf39ff
Владимир Корнев <78905217+VKornev22@users.noreply.github.com>
2021-04-05 14:05:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
+  int count = 0;
+    for (int i = 0; i < len-1; i++) {
+        for (int j = i+1; j < len; j++) {
+            if (arr[i] + arr[j] == value)
+                count++;
+        }
+    }
+    if(count)
+      return count;
+    else
   return 0;
 }
 
