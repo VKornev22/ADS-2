258b4c8b7d6da6bb45505929494c45f07047ed3e
Владимир Корнев <78905217+VKornev22@users.noreply.github.com>
2021-04-09 19:22:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,9 +15,39 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = len-1; i > 0; i--) {
+    if (arr[len-1] <= value) {
+    for (int j = i; j > 0; j--)
+      if (arr[i] + arr[j-1] == value)
+        count++;
+    }
+    else 
+      len--;
+  }
+    if (count)
+      return count;
+    else
+      return 0;
 }
 
 int countPairs3(int *arr, int len, int value) {
+  int n = 0;
+  for (int i = 0; i < len - 1; i++) {
+  int left = i, right = len;
+    while (left < right-1) {
+    int midd = (left + right) / 2;
+      if (arr[i] + arr[midd] == value) {
+      n++;
+      int j = midd+1;
+        while (arr[i] + arr[j] == value && j < right) {
+        n++;
+        j++;
+        }
+      j = midd - 1;
+          while (arr[i] + arr[j] == value && j > left) {
+          n++;
+          j--;
+          }
   return 0;
 }
